ea6cf35bbbab84ca15366fd17ae8e7b850ad084e
Jon O'Doherty <jonodee@gmail.com>
2021-12-11 18:51:34 +0000

    updates the database location and puts environment variables

--- main.py
+++ main.py
@@ -70,8 +70,8 @@ class Comment(db.Model):
     post_id = db.Column(db.Integer, db.ForeignKey('blog_posts.id'))
     parent_post = relationship("BlogPost", back_populates = "comments" )
 
-
-db.create_all()
+#
+# db.create_all()
 
 
 ##CREATE TABLE IN DB
